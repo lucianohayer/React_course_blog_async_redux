50e99d7c9279033c54032f960dea8c1f30a3eeb1
Luciano Hayer <lucianohayer@gmail.com>
2021-08-28 03:24:24 +0200

    fetch users data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach((id) => dispatch(fetchUser(id)))
+		.value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
 	let posts = [];
@@ -12,5 +23,21 @@ export const fetchPosts = () => async (dispatch) => {
 	dispatch({
 		type: 'FETCH_POSTS',
 		payload: posts,
+	});
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+	let user = null;
+
+	try {
+		const response = await jsonPlaceholder.get(`/users/${id}`);
+		user = response.data;
+	} catch (err) {
+		console.error(err);
+	}
+
+	dispatch({
+		type: 'FETCH_USER',
+		payload: user,
 	});
 };
